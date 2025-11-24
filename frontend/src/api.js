85284e9fc535d558cf799fb1fc98be9355551bfb
Inna <innanuzhdina7@gmail.com>
2025-11-24 17:44:07 +0000

    Fix API_URL export to remove VITE_API_URL prefix
    Signed-off-by: Inna <innanuzhdina7@gmail.com>

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "VITE_API_URL=https://api.спкхорошово-1.рф";
+export const API_URL = "https://api.спкхорошово-1.рф";
 
 const api = axios.create({
   baseURL: API_URL,
