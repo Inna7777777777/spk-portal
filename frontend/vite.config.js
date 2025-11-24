43f64c472859b67260efbdfffb6bdf866caa6367
Inna <innanuzhdina7@gmail.com>
2025-11-24 13:32:25 +0000

    Change React plugin import to remote URL
    Replaced local React plugin import with a remote URL.
    
    Signed-off-by: Inna <innanuzhdina7@gmail.com>

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "https://spk-portal-frontend.onrender.com";
 
 export default defineConfig({
   plugins: [react()],
