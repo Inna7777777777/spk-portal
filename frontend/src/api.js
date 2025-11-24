f9060d42e5997f23164445de01c06a4e6846bff6
Inna <innanuzhdina7@gmail.com>
2025-11-24 17:06:46 +0000

    Change API_URL to production endpoint
    Signed-off-by: Inna <innanuzhdina7@gmail.com>

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://127.0.0.1:8000";
+export const API_URL = "https://spk-portal-backend.onrender.com";
 
 const api = axios.create({
   baseURL: API_URL,
